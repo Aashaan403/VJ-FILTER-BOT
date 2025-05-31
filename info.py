4992eb48d2b21b6d7e0ce6dcccaea9296524d4db
Aswin Parames S S <164914083+Aashaan403@users.noreply.github.com>
2025-05-31 23:18:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -89,7 +89,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+7zBuguMnwahiNjY1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+EInNL8Rx9gQyM2Jl')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+-_Suxrg8SKZkM2I1')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+uOpaanjtaKYyYWQ1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Aashaan100')
 
